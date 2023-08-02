bb27cf184cf5e0a6db820d58d54551b9b0174a95
Andrew <mullinsa428@gmail.com>
2023-08-01 22:33:21 -0400

    first draft

--- frontend/src/components/ElCapScene.jsx
+++ frontend/src/components/ElCapScene.jsx
@@ -1,18 +1,29 @@
 import { Suspense, useRef, useContext, useEffect } from 'react'
-import { CameraControls, Box } from "@react-three/drei"
+import * as THREE from 'three';
+import { CameraControls, Box, Circle } from "@react-three/drei"
 import { useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import CameraPositionContext from '../CameraPositionContext'
 import SelectedObjectContext from '../SelectedObjectContext'
 import { ObjectHighlight } from './ObjectHighlight'
-
+import pic from '../../public/yosemite_top_map.png'
 
 export const ElCapScene = () => {
 
     const gltf = useLoader(GLTFLoader, '/el_capitan/scene.gltf')
+    const floorMap = useLoader(THREE.TextureLoader, pic)
     const { cameraPosition, setCameraPosition } = useContext(CameraPositionContext)
     const { selectedObject } = useContext(SelectedObjectContext)
     const cameraControlsRef = useRef()
+    const elCap = useRef()
+
+    useEffect(()=> {
+        const cameraBoundary = new THREE.Box3(
+            new THREE.Vector3( -7.0,-1.5, -1.5 ),
+            new THREE.Vector3( 7.0, 10.0, 10.0 )
+        );
+        cameraControlsRef.current?.setBoundary(cameraBoundary)
+    },[])
     
     // whenever the cameraPositionContext is changed, this will adjust the camera accordingly
     useEffect(() => {
@@ -31,21 +42,33 @@ export const ElCapScene = () => {
     },[selectedObject])
 
 
+    const cameraBox = () => {
+        
+    } 
+
+
+
     return (
         <>
-            <primitive object={gltf.scene} rotation={[ 0, -Math.PI/3, 0]} scale={7}/>
+            <primitive useRef={elCap} object={gltf.scene} rotation={[ 0, -Math.PI/3, 0]} scale={7}/>
             <ObjectHighlight />
+
             <CameraControls
                 ref={cameraControlsRef}
                 enabled= {true}
                 verticalDragToForward={false}
                 enableTransition
-                maxDistance={8}
+                maxDistance={15}
                 minDistance={1}
                 maxPolarAngle={3*Math.PI/4}
-                // colliderMeshes={[primitive]}
+                boundaryEnclosesCamera={true}
             />
             <hemisphereLight intensity={1}/>
+            <mesh position={[9, -1.74, 7.5]} rotation={[-Math.PI/2, 0, -Math.PI/24]}> 
+                <planeGeometry attach="geometry" args={[47.62, 25]} />
+                <meshStandardMaterial map={floorMap} roughness={1}/>
+            </mesh> 
+            {/* <Circle position={[0, -1.74, 0]} args={[100,0.1,100]}/> */}
             {/* <spotLight position={[0,7,0]} intensity={1} /> */}
         </>
     )
